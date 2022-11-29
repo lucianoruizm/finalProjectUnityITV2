53146ba01403b9e9f091f375704131b81f6aa864
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-29 00:23:07 -0300

    rotate player

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,35 +6,63 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float speed = 5f;
+    float deltaRotation = 30f;
+    float deltaMovement = 7f;
     
     void FixedUpdate()
     {
-        Vector3 movementInput = Vector3.zero;
+        // Vector3 movementInput = Vector3.zero;
 
-        if(Input.GetKey(KeyCode.W))
-        {
-            movementInput.z = 1;
-        }
-        else if(Input.GetKey(KeyCode.S))
-        {
-            movementInput.z = -1;
-        }
+        // if(Input.GetKey(KeyCode.W))
+        // {
+        //     movementInput.z = 1;
+        // }
+        // else if(Input.GetKey(KeyCode.S))
+        // {
+        //     movementInput.z = -1;
+        // }
 
         
+        // if(Input.GetKey(KeyCode.D))
+        // {
+        //     movementInput.x = 1;
+        // }
+        // else if(Input.GetKey(KeyCode.A))
+        // {
+        //     movementInput.x = -1;
+        // }
+
+        // Move(movementInput);
+        Movement();
+        Rotate();
+    }
+
+    // void Move(Vector3 direction)
+    // {
+    //     transform.position += direction.normalized * speed * Time.deltaTime;
+    // }
+
+    void Rotate ()
+    {
         if(Input.GetKey(KeyCode.D))
         {
-            movementInput.x = 1;
+            transform.Rotate(new Vector3(0f, deltaRotation, 0f) * speed * Time.deltaTime);
         }
         else if(Input.GetKey(KeyCode.A))
         {
-            movementInput.x = -1;
+            transform.Rotate(new Vector3(0f, -deltaRotation, 0f) * speed * Time.deltaTime);
         }
-
-        Move(movementInput);
     }
 
-    void Move(Vector3 direction)
+    void Movement ()
     {
-        transform.position += direction.normalized * speed * Time.deltaTime;
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * deltaMovement * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * deltaMovement * Time.deltaTime);
+        }
     }
 }
