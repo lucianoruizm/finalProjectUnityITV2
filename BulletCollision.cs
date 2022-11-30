3b315db1a37b06a45f67998caf0f6289ce14fc36
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-30 00:01:30 -0300

    gun, sound, music

--- BulletCollision.cs
+++ BulletCollision.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class BulletCollision : MonoBehaviour
 {
+    [SerializeField] GameObject explotion;
+    [SerializeField] AudioSource asExplotionSound;
+    [SerializeField] AudioClip explotionSound;
+
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("Enemy"))
         {   
+            Instantiate(explotion, other.gameObject.transform.position, other.gameObject.transform.rotation);
+
+            asExplotionSound.PlayOneShot(explotionSound);
+            
             Destroy(other.gameObject);
             
             Destroy(gameObject, 2.0f);
