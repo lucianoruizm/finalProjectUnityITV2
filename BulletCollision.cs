723e4101253da8136893c3254f45295fb4938907
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-11-30 23:07:23 -0300

    enemy health

--- BulletCollision.cs
+++ BulletCollision.cs
@@ -7,18 +7,45 @@ public class BulletCollision : MonoBehaviour
     [SerializeField] GameObject explotion;
     [SerializeField] AudioSource asExplotionSound;
     [SerializeField] AudioClip explotionSound;
+    public int enemyHealth = 0;
 
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("Enemy"))
         {   
-            Instantiate(explotion, other.gameObject.transform.position, other.gameObject.transform.rotation);
-
-            asExplotionSound.PlayOneShot(explotionSound);
-            
-            Destroy(other.gameObject);
-            
+            enemyHealth++;
+            Debug.Log(enemyHealth);
             Destroy(gameObject, 2.0f);
+
+            if (enemyHealth == 3)
+            {
+              Instantiate(explotion, other.gameObject.transform.position, other.gameObject.transform.rotation);
+  
+              asExplotionSound.PlayOneShot(explotionSound);
+              
+              Destroy(other.gameObject);
+            }
+            
         }
     }
+
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     damage = damage + 1;
+        
+    //     if(other.gameObject.CompareTag("EnemyBoss"))
+    //     {   
+    //         if(damage == 3)
+    //         {
+    //             Instantiate(explotion, other.gameObject.transform.position, other.gameObject.transform.rotation);
+
+    //             asExplotionSound.PlayOneShot(explotionSound);
+                
+    //             Destroy(other.gameObject);
+                
+    //             Destroy(gameObject, 2.0f);
+    //         }
+
+    //     }
+    // }
 }
